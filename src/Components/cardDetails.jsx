3553638f8e41fa823f12e80415a48c085f8cff76
Alex <alexsmith11nd@gmail.com>
2020-02-15 21:50:30 +0000

    Added css, card and card details

--- src/Components/cardDetails.jsx
+++ src/Components/cardDetails.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import img3 from '../img/mountain.jpg';
+import "./card.css";
+
+/**
+ * individual card component. Saves code reuse.
+ * Bootstrap is used for almost all early styling - I may have later changed.
+ * @param {*} props 
+ */
 
 const Card = props => {
     return (
-        <div className="card text-cen">
+        <div className="card text-center shadow">
             <div className="overflow">
-                <img src={img3} alt="Image 3 - Mountain" />
+                <img src={props.imgsrc} alt="Image 3 - Mountain" className='card-img-top' />
             </div>
             <div className="card-body text-dark">
-                <h4 className="card-title">Title of the Card</h4>
-                <p className="card-text text-secondary">
-                    Hello this is really cool
-                </p>
+                <h4 className="card-title">{props.title}</h4>
+                <p className="card-text text-secondary">{props.description}</p>
                 <a href="#" className="btn btn-outline-success">Go anywhere</a>
             </div>
         </div>
--- src/Components/cards.jsx
+++ src/Components/cards.jsx
@@ -1,19 +1,48 @@
 import React, { component, Component } from 'react';
 import Card from './cardDetails';
 
+/**
+ * Import images
+ */
+import img1 from '../img/mountain.jpg'
+import img2 from '../img/lake.jpg'
+import img3 from '../img/desert.jpg'
+
+/**
+ * Top level comp for the cards. Could control how many, where, etc.
+ * We create a grid using bootstrap rows/columns.
+ * We also pass in the props unique to each card.
+ */
 class Cards extends Component {
+
+    componentDidMount() {
+        fetch('')
+    }
+
     render() {
         return (
             <div className="container-fluid d-flex justify-content-center">
                 <div className="row">
                     <div className="col-md-4">
-                        <Card/>
+                        <Card
+                        imgsrc={img1}
+                        title="Mountain"
+                        description="this is a mountain"
+                        />
                     </div>
                     <div className="col-md-4">
-                        <Card/>
+                        <Card
+                        imgsrc={img2}
+                        title="Lake"
+                        description="this is a lake"
+                        />
                     </div>
                     <div className="col-md-4">
-                        <Card/>
+                        <Card
+                        imgsrc={img3}
+                        title="Desert"
+                        description="this is a desert"
+                        />
                     </div>
                 </div>
             </div>
